c34a153e776f672c228abca978f8cc38649724fe
JozeRamos <up202005460@up.pt>
2021-12-29 18:58:04 +0000

    hero added

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -10,11 +10,11 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
-    private int x = 10;
-    private int y = 10;
+    private final Hero hero;
     private TerminalScreen screen;
 
     public Game(){
+        hero = new Hero(10, 10);
         try {
             TerminalSize terminalSize = new TerminalSize(40, 20);
             DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
@@ -32,24 +32,23 @@ public class Game {
         System.out.println(key);
         switch (key.getKeyType()) {
             case ArrowUp:
-                y--;
+                hero.moveUp();
                 break;
             case ArrowDown:
-                y++;
+                hero.moveDown();
                 break;
             case ArrowRight:
-                x++;
+                hero.moveRight();
                 break;
             case ArrowLeft:
-                x--;
+                hero.moveLeft();
                 break;
         }
 
     }
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(x, y,
-                TextCharacter.fromCharacter('X')[0]);
+        hero.draw(screen);
         screen.refresh();
     }
     public void run() throws IOException {
---
+++ src/main/java/Hero.java
@@ -0,0 +1,53 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.TerminalScreen;
+
+public class Hero {
+    private int x;
+    private int y;
+
+    public Hero(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Hero moveUp(){
+        setY(y-1);
+        return new Hero(x,y);
+    }
+
+    public Hero moveDown(){
+        setY(y+1);
+        return new Hero(x,y);
+    }
+
+    public Hero moveRight(){
+        setX(x+1);
+        return new Hero(x+1,y);
+    }
+
+    public Hero moveLeft(){
+        setX(x-1);
+        return new Hero(x-1,y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void draw(TerminalScreen screen) {
+        screen.setCharacter(x, y,
+                TextCharacter.fromCharacter('X')[0]);
+    }
+}
